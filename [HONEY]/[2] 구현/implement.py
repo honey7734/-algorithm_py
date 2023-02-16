a632acc7b3fca1aa5da661334b521024b4f58303
YEA HYEON UI <gusdml7243@gmail.com>
2023-02-16 21:13:36 +0900

    [2] 구현_ 상하좌우 문제, 시각 문제 (예제)

---
+++ [HONEY]/[2] 구현/implement.py
@@ -0,0 +1,32 @@
+# 구현 4-1 상하좌우 문제
+
+n = int(input())
+plans = input().split()
+x, y = 1, 1
+
+move_types = ['L', 'R', 'U', 'D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+for plan in plans:
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
+
+# 구현 4-2 시각
+n = int(input())
+
+count = 0
+for i in range(n+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
